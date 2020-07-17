7f177604edf98f72a407a52fabdb3b1f1c520a0a
AndriusVegan <lifecoachandy7@gmail.com>
2020-07-17 12:46:49 +0100

    made mobile friendly, need to fix a bug as chart now showing up

--- script.js
+++ script.js
@@ -4,32 +4,31 @@ window.onload = () => {
   getWorldCoronaData();
   // $('.ui.dropdown').dropdown();
 
-
   // document.querySelector(".active-cases-card").addEventListener("click", () => {
   //   console.log("yo we clicked");
-  // }); 
-  // use Html inlin onclick instrad
-}
+  // });
+  // use Html inline onclick instead
+};
 
 var map;
 var infoWindow;
 let coronaGlobalData;
 let mapCircles = [];
 const worldwideSelection = {
-  name: 'Worldwide',
-  value: 'www',
-  selected: true
-}
+  name: "Worldwide",
+  value: "www",
+  selected: true,
+};
 var casesTypeColor = {
-  cases: "#1d2c4d",
+  cases: "#cc1034",
   active: "#9d80fe",
   recovered: "#7dd71d",
   deaths: "#fb4443",
 };
 const mapCenter = {
   lat: 34.80746,
-  lng: -40.4796
-}
+  lng: -40.4796,
+};
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: mapCenter,
@@ -39,46 +38,44 @@ function initMap() {
   infoWindow = new google.maps.InfoWindow();
 }
 
-const changeDataSelection = (casesType) => {
+const changeDataSelection = (elem, casesType) => {
   clearTheMap();
   showDataOnMap(coronaGlobalData, casesType);
   setActiveTab(elem);
 };
 
+const setActiveTab = (elem) => {
+  const activeEL = document.querySelector(".card.active");
+  activeEL.classList.remove("active");
+  elem.classList.add("active");
+};
+
 const clearTheMap = () => {
   for (let circle of mapCircles) {
     circle.setMap(null);
-
   }
 };
-
-const setActiveTab = (elem) =>{
-  const activeEL = document.querySelector('.acrd.active');
-  activeEL.classList.remove('active')
-  elem.classList.add('active')
-}
 
 const setMapCenter = (lat, long, zoom) => {
   map.setZoom(zoom);
   map.panTo({
     lat: lat,
-    lng: long
-  })
-}
-
+    lng: long,
+  });
+};
 
 const initDropdown = (searchList) => {
-  $('.ui.dropdown').dropdown({
+  $(".ui.dropdown").dropdown({
     values: searchList,
-    onChange: function(value, text) {
-      if(value !== worldwideSelection.value){
+    onChange: function (value, text) {
+      if (value !== worldwideSelection.value) {
         getCountryData(value);
       } else {
         getWorldCoronaData();
       }
-    }
+    },
   });
-}
+};
 
 const setSearchList = (data) => {
   let searchList = [];
@@ -86,16 +83,16 @@ const setSearchList = (data) => {
   data.forEach((countryData) => {
     searchList.push({
       name: countryData.country,
-      value: countryData.countryInfo.iso3
-    })
-  })
+      value: countryData.countryInfo.iso3,
+    });
+  });
   initDropdown(searchList);
-}
+};
 
 const getCountriesData = () => {
   fetch("https://disease.sh/v2/countries")
     .then((response) => {
-      return response.json()
+      return response.json();
     })
     .then((data) => {
       coronaGlobalData = data;
@@ -105,47 +102,49 @@ const getCountriesData = () => {
     });
 };
 
-const getCountryData = (countryIso) =>{
+const getCountryData = (countryIso) => {
   const url = "https://disease.sh/v3/covid-19/countries/" + countryIso;
   fetch(url)
-  .then((response) => {
-    return response.json()
-  })
-  .then((data) => {
-    setMapCenter(data.countryInfo.lat, data.countryInfo.long, 3);
-    setStatsData(data);
-  });
-}
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      setMapCenter(data.countryInfo.lat, data.countryInfo.long, 3);
+      setStatsData(data);
+    });
+};
 
 const getWorldCoronaData = () => {
   fetch("https://disease.sh/v2/all")
     .then((response) => {
       return response.json();
     })
-    .then((data)=>{
+    .then((data) => {
       // let chartData = buildChartData(data);
       buildPieChart(data);
-      //if you dont use pie chart need to comment out as it would not go to next
+      //if you don't use pie chart need to comment out as it would not go to next
       setStatsData(data);
       setMapCenter(mapCenter.lat, mapCenter.lng, 2);
-    })
+    });
 };
 
 const setStatsData = (data) => {
-  let addedCases = numeral(data.todayCases).format('+00');
-  let addedRecovered = numeral(data.todayRecovered).format('+00');
-  let addedDeaths = numeral(data.todayDeaths).format('+00');
-  let totalCases = numeral(data.cases).format('0.0a');
-  let totalRecovered = numeral(data.recovered).format('0.0a');
-  let totalDeaths = numeral(data.deaths).format('0.0a');
+  let addedCases = numeral(data.todayCases).format("+00");
+  let addedRecovered = numeral(data.todayRecovered).format("+00");
+  let addedDeaths = numeral(data.todayDeaths).format("+00");
+  let totalCases = numeral(data.cases).format("0.0a");
+  let totalRecovered = numeral(data.recovered).format("0.0a");
+  let totalDeaths = numeral(data.deaths).format("0.0a");
 
-  document.querySelector('.total-number').innerHTML=addedCases;
-  document.querySelector('.recovered-number').innerHTML=addedRecovered;
-  document.querySelector('.deaths-number').innerHTML=addedDeaths;
-  document.querySelector('.cases-total').innerHTML=`${totalCases} Total`;
-  document.querySelector('.recovered-total').innerHTML=`${totalRecovered} Total`;
-  document.querySelector('.deaths-total').innerHTML=`${totalDeaths} Total`;
-}
+  document.querySelector(".total-number").innerHTML = addedCases;
+  document.querySelector(".recovered-number").innerHTML = addedRecovered;
+  document.querySelector(".deaths-number").innerHTML = addedDeaths;
+  document.querySelector(".cases-total").innerHTML = `${totalCases} Total`;
+  document.querySelector(
+    ".recovered-total"
+  ).innerHTML = `${totalRecovered} Total`;
+  document.querySelector(".deaths-total").innerHTML = `${totalDeaths} Total`;
+};
 
 const getHistoricalData = () => {
   fetch("https://disease.sh/v2/historical/all?lastdays=120")
@@ -226,9 +225,9 @@ const showDataInTable = (data) => {
     html += `
       <tr>
           <td>${country.country}</td>
-          <td>${numeral(country.cases).format('0,0')}</td>
-          <td>${numeral(country.recovered).format('0.0')}</td>
-          <td>${numeral(country.deaths).format('0.0a')}</td>
+          <td>${numeral(country.cases).format("0,0")}</td>
+          <td>${numeral(country.recovered).format("0.0")}</td>
+          <td>${numeral(country.deaths).format("0.0a")}</td>
       </tr>
       `;
   });
